43a0c70fcb167a3376fbebbb990821ee5c54eef8
zhengyiheng <feihukings@qq.com>
2018-06-29 14:15:50 +0800

    修改两个类

--- app/src/main/java/com/example/administrator/myprogect/Test_zheng2.java
+++ app/src/main/java/com/example/administrator/myprogect/Test_zheng2.java
@@ -1,4 +1,31 @@
 package com.example.administrator.myprogect;
 
 public class Test_zheng2 {
+    private String d;
+    private String e;
+    private String f;
+
+    public String getD() {
+        return d;
+    }
+
+    public void setD(String d) {
+        this.d = d;
+    }
+
+    public String getE() {
+        return e;
+    }
+
+    public void setE(String e) {
+        this.e = e;
+    }
+
+    public String getF() {
+        return f;
+    }
+
+    public void setF(String f) {
+        this.f = f;
+    }
 }
